e51378d5d637fe4e00db5af053293661554cfbd3
jp <jeanphilippe.fong@gmail.com>
2017-09-24 09:30:42 -0400

    change app and add api file

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,10 @@
+const api = "localhost:3001"
+
+const headers = {
+  'Authorization': 'whatever-you-want'
+}
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, headers)
+    .then(res => res.json())
+    .then(data => data.categories)
