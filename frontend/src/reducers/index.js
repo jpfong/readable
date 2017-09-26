6a664bd041e179b09d42b86486d79e3b3c354470
jp <jeanphilippe.fong@gmail.com>
2017-09-25 21:44:46 -0400

    add reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,35 @@
 import { combineReducers } from 'redux'
 
-export default combineReducers({
+import {
+  RECEIVE_CATEGORIES
+} from '../actions/categories'
 
+import {
+  RECEIVE_POSTS,
+  RECEIVE_CATEGORY_POSTS
+} from '../actions/posts'
+
+function categories (state = [], action) {
+  switch (action.type) {
+    case RECEIVE_CATEGORIES :
+      return action.categories
+    default :
+      return state
+  }
+}
+
+function posts (state = [], action ) {
+  switch (action.type) {
+    case RECEIVE_POSTS :
+      return action.posts
+    case RECEIVE_CATEGORY_POSTS :
+      return action.posts
+    default :
+      return state
+  }
+}
+
+export default combineReducers({
+  categories,
+  posts
 })
